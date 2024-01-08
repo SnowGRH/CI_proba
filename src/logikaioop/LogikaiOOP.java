76484cb1ec2c642477cc2060c0ef21c19b666e7a
szlankapeter <szlankapeter@gmail.com>
2024-01-08 13:51:19 +0100

    Bekeres

--- src/logikaioop/LogikaiOOP.java
+++ src/logikaioop/LogikaiOOP.java
@@ -1,14 +1,19 @@
 package logikaioop;
 
-public class LogikaiOOP {
+import java.util.Scanner;
 
+public class LogikaiOOP {
+    
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Leírás");
         System.out.println("Az 1. állítás");
         System.out.println("A 2. állítás");
         System.out.println("A 3. állítás");
         System.out.println("Melyik");
+        String be = sc.nextLine();
         System.out.println("A válasz jó | rossz");
+        
     }
     
 }
