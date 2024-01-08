13d035b4bb6cb51acbed07b061c1d14de8e4fbe8
szlankapeter <szlankapeter@gmail.com>
2024-01-08 14:50:42 +0100

    Lada

---
+++ src/logikaioop/Lada.java
@@ -0,0 +1,41 @@
+package logikaioop;
+
+public class Lada {
+
+    private String allitas, anyag;
+    private boolean kincs;
+
+    public Lada() {
+        this.allitas = "Allitas 1";
+    }        
+
+    public String getAllitas() {
+        return allitas;
+    }
+
+    public String getAnyag() {
+        return anyag;
+    }
+
+    public boolean isKincs() {
+        return kincs;
+    }
+
+    public void setAllitas(String allitas) {
+        this.allitas = allitas;
+    }
+
+    public void setAnyag(String anyag) {
+        this.anyag = anyag;
+    }
+
+    public void setKincs(boolean kincs) {
+        this.kincs = kincs;
+    }
+
+    @Override
+    public String toString() {
+        return allitas;
+    }    
+
+}
